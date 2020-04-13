9fe9e7ca2440e5945ece3ac2d8272608de488cec
Frozen Feelings <kiprutohcollo@gmail.com>
2020-04-13 13:41:33 +0300

    imported github component in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { GithubComponent } from "./components/github/github.component";
+
+const routes: Routes = [{ path: "github", component: GithubComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
